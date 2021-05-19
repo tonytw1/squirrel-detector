afd43a8a1d60f3e30f2c2fe8e1743a3d5bcaa66b
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-05-19 18:49:09 +0100

    Export Motion image and metadata to a MQTT message

--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 Motion does a good job of detecting movement and bounding boxes.
 
 
-Let's use a pyhton script to response to this event and publish the image and data about the event.
-`on_motion_detected.py`
-This script want to capture the image file path and the bounding box and
-try to encode both of these into a message.
+Let's use a python script to catch this event and publish the image and event data.
 
-The `on_picture_save` event seems to be able to give us the image file and bounding box.
+`on_motion_detected.py`
+
+This script needs capture the image file path and the bounding box from Motion 
+and encode them into a message.
+
+The Motion `on_picture_save` event is able to give us the image file path and bounding box.
 We can hook these together with this configuration line:
 
 `on_picture_save python /home/pi/on_motion_detected.py %f %w %h %K %L %i %J`
+
+
+We want to open the binary image file and encoded it for inclution on a message.
+base64 encoding should be enough.
+
+
 
--- on_motion_detected.py
+++ on_motion_detected.py
@@ -1,10 +1,31 @@
-
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 import os
+import subprocess
+import json
 
-message = ""
-for arg in sys.argv[1:]:
-        message += arg + ":"
+# Do we have a readable image file path?
+image_filepath = sys.argv[1];
 
-os.system("mosquitto_pub -t motion -m " + message)
+
+if os.access(image_filepath, os.R_OK):
+        # Encode it to a base64 string
+        result = subprocess.run(['base64', image_filepath], stdout=subprocess.PIPE)
+        image = result.stdout.decode('utf-8')
+        message = ""
+        for arg in sys.argv[2:]:
+                message += arg + ":"
+
+        message = {
+                'image_file': image_filepath,
+                'image_width': sys.argv[2],
+                'image_height': sys.argv[3],
+                'bounding_box_center_x': sys.argv[4],
+                'bounding_box_center_y': sys.argv[5],
+                'bounding_box_width': sys.argv[6],
+                'bounding_box_height': sys.argv[7],
+                'image': image
+        }
+
+        subprocess.run(['mosquitto_pub', '-t', 'motion', '-m', json.dumps(message)])
+
