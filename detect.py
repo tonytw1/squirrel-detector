f7f1b62fc0882b28cb534a879339642d978ba26b
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-05-22 12:19:13 +0100

    Check timings of the various steps; TensorFlow import and model load are slower than detect

--- detect.py
+++ detect.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 
+import sys 
 import PIL.Image
 import numpy
+
+image_file = sys.argv[1]
+# Convert our image to a numpy array
+image = PIL.Image.open(image_file)
+np_image = numpy.array(image)
+
+print("Importing TensorFlow")
 import tensorflow as tf
 
-saved_model = tf.saved_model.load('models/ssd_mobilenet_v2_320x320_coco17_tpu-8/saved_model/')
+print("Loading saved model")
+saved_model = tf.saved_model.load('/home/tony/.keras/datasets/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved_model/')
 model = saved_model.signatures['serving_default'] #TODO This looks important and is not understood
-
-# Convert our image to a numpy array
-image = PIL.Image.open("squirrel.jpg")
-np_image = numpy.array(image)
 
 input_tensor = tf.convert_to_tensor(np_image)
 input_tensor = input_tensor[tf.newaxis, ...]
 
+print("Detecting")
 detections = model(input_tensor)	# TODO doesn't match the .predict() method call in the docs
-
-num_of_detections = int(detections['num_detections'].numpy())
-for i in range(0, num_of_detections):
+num = int(detections['num_detections'].numpy())
+for i in range(0, num):
 	detection_class = detections['detection_classes'][0][i].numpy();
 	detection_score = detections['detection_scores'][0][i].numpy();
 	print(str(detection_class) + ": " + str(detection_score))
