f185d035a420152be403ea8a13f00b5b5d2b11d5
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-05-25 22:38:43 +0100

    Setting up for initial export

---
+++ retraining/export.bash
@@ -0,0 +1,2 @@
+python3 exporter_main_v2.py --input_type image_tensor --pipeline_config_path ./training/pipeline.config --trained_checkpoint_dir ./training/ --output_directory exported/squirrelnet
+
