5f4369bf7b37fb26f786cc55773294aea9c5f1bc
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-05-25 12:15:19 +0100

    Listener resolves class labels

--- listener.py
+++ listener.py
@@ -15,6 +15,31 @@ topic = 'motion'
 tensorflowserver_url = "http://localhost:32701/v1"
 model = "ssd_mobilenet_v2_320x320_coco17_tpu-8"
 
+from google.protobuf import text_format
+import string_int_label_map_pb2
+from six import string_types
+
+# Parse a labels protobuf file into a python map
+# Adapted from retrain/models/research/object_detection/utils/label_map_util.py
+def load_labelmap(path):
+  with open (path, "r") as labels_file:
+    label_map_string=labels_file.read()
+    label_map = string_int_label_map_pb2.StringIntLabelMap()
+    try:
+      text_format.Merge(label_map_string, label_map)
+    except text_format.ParseError:
+      label_map.ParseFromString(label_map_string)
+  return label_map
+
+def get_labels(label_map_path):
+  label_map = load_labelmap(label_map_path)
+  label_map_dict = {}
+  for item in label_map.item:
+      label_map_dict[item.id] = item.display_name
+  return label_map_dict
+
+labels = get_labels('mscoco_label_map.pbtxt')
+
 # Given a PIL image call TensorFlow Serving to detect objects
 def predict(image):
 	image_np = numpy.array(image)
@@ -44,7 +69,7 @@ def on_message(client, userdata, msg):
     predictions = predict(image)
 
     print("Got predicitions: ")
-    prediction = predictions[0]
+    prediction = predictions[0];
     detection_scores = prediction['detection_scores']
     detection_classes = prediction['detection_classes']
 
@@ -52,7 +77,8 @@ def on_message(client, userdata, msg):
     detected_classes = numpy.array(list(zip(detection_classes, detection_scores)))
     top = detected_classes[0:5]
     for i in range(len(top)):
-        print(str(top[i][0]) + ": " + str(top[i][1]))
+        label_display_name = labels[top[i][0]]
+        print(label_display_name + " (" + str(top[i][0]) + "): " + str(top[i][1]))
 
 client = mqtt.Client()
 client.on_connect = on_connect
