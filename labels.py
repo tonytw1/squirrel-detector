f0b0ad2af1913ae885037c8528f373cdd85970a7
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-05-25 11:56:12 +0100

    Clean up; can read labels file without needing to bring in a TensorFlow import

--- labels.py
+++ labels.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import tensorflow as tf
 from google.protobuf import text_format
 import string_int_label_map_pb2
 from six import string_types
@@ -8,8 +7,8 @@ from six import string_types
 # Parse a labels protobuf file into a python map
 # Adapted from retrain/models/research/object_detection/utils/label_map_util.py
 def load_labelmap(path):
-  with tf.io.gfile.GFile(path, 'r') as fid:
-    label_map_string = fid.read()
+  with open (path, "r") as labels_file:
+    label_map_string=labels_file.read()
     label_map = string_int_label_map_pb2.StringIntLabelMap()
     try:
       text_format.Merge(label_map_string, label_map)
