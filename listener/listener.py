4a830a49bed9bfcca281bf2e0b1790252b5f8349
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-02-06 17:21:55 +0000

    Refactor; extract send email

--- listener/listener.py
+++ listener/listener.py
@@ -7,7 +7,7 @@
 #   'image_file': Original path to the image file on camera device
 # }
 #
-# Foreach motion message run TensorFlow object detection and publish the highest class scores 
+# Foreach motion message run TensorFlow object detection and publish the highest class scores
 # {
 #   'detections': {} # Map of class detection scoress
 #   'image': BASE64 encoded JPEG
@@ -16,6 +16,7 @@
 # onto another MQTT detections topic
 # If the detection score is high enough send a notification (email) showing the detection.
 
+import tensorflow as tf
 import paho.mqtt.client as mqtt
 
 from io import BytesIO
@@ -43,10 +44,10 @@ from six import string_types
 
 import logging
 import sys
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s')
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format='%(asctime)s %(levelname)-8s %(message)s')
 
 logging.info("Importing TensorFlow")
-import tensorflow as tf
 logging.info("TensorFlow imported")
 
 broker = os.environ.get('MOTION_MQTT_HOST')
@@ -71,36 +72,43 @@ last_sent = 0
 
 # Load the model
 logging.info("Loading model")
-saved_model = tf.saved_model.load('../models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved_model/')
+saved_model = tf.saved_model.load(
+    './models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved_model/')
 model = saved_model.signatures['serving_default']
 logging.info("Model loaded")
 
 # Parse a labels protobuf file into a python map
 # Adapted from retrain/models/research/object_detection/utils/label_map_util.py
+
+
 def load_labelmap(path):
-  with open (path, "r") as labels_file:
-    label_map_string=labels_file.read()
-    label_map = string_int_label_map_pb2.StringIntLabelMap()
-    try:
-      text_format.Merge(label_map_string, label_map)
-    except text_format.ParseError:
-      label_map.ParseFromString(label_map_string)
-  return label_map
+    with open(path, "r") as labels_file:
+        label_map_string = labels_file.read()
+        label_map = string_int_label_map_pb2.StringIntLabelMap()
+        try:
+            text_format.Merge(label_map_string, label_map)
+        except text_format.ParseError:
+            label_map.ParseFromString(label_map_string)
+    return label_map
+
 
 def get_labels(label_map_path):
-  label_map = load_labelmap(label_map_path)
-  label_map_dict = {}
-  for item in label_map.item:
-      label_map_dict[item.id] = item.name
-  return label_map_dict
+    label_map = load_labelmap(label_map_path)
+    label_map_dict = {}
+    for item in label_map.item:
+        label_map_dict[item.id] = item.name
+    return label_map_dict
 
 # Given a PIL image call TensorFlow to detect objects
+
+
 def predict_tf(image_np):
-	input_tensor = tf.convert_to_tensor(image_np)
-	input_tensor = input_tensor[tf.newaxis, ...]
-	logging.info("Detecting")
-	prediction = model(input_tensor)
-	return prediction
+    input_tensor = tf.convert_to_tensor(image_np)
+    input_tensor = input_tensor[tf.newaxis, ...]
+    logging.info("Detecting")
+    prediction = model(input_tensor)
+    return prediction
+
 
 def on_connect(client, userdata, flags, rc):
     logging.info("Connected with result code "+str(rc))
@@ -108,18 +116,22 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     client.subscribe(topic)
 
+
 labels = get_labels(label_file)
 
-def send_detection_message(detections, image, annotated_image):
+
+def send_detection_message(detections, image, annotated_image, duration):
     message = {
-       'detections': detections,
-       'image': image,
-       'annotated_image': annotated_image
+        'detections': detections,
+        'image': image,
+        'annotated_image': annotated_image,
+        'duration': duration
     }
-    detection_message = json.dumps(message) 
+    detection_message = json.dumps(message)
     logging.info("Sending detection message")
     # TODO stream to mqtt
     client.publish(detections_topic, detection_message)
+
 
 def send_zeros():
     global last_detection
@@ -128,55 +140,101 @@ def send_zeros():
         zeros = {}
         for c in labels.values():
             zeros[c] = 0.0
-        send_detection_message(zeros, None, None)
+        send_detection_message(zeros, None, None, None)
+
 
 def annotateImage(prediction, image, image_np):
-        # Given a prediction and the numpy image
-        # annotate that image with a bounding box for the best prediction
-        # Returns a JPEG byte array
-        detection_boxes = prediction['detection_boxes'].numpy().tolist()
-        detection_scores = prediction['detection_scores'].numpy().tolist()[0]
-        detection_classes = prediction['detection_classes'].numpy().tolist()[0]
+    # Given a prediction and the numpy image
+    # annotate that image with a bounding box for the best prediction
+    # Returns a JPEG byte array
+    detection_boxes = prediction['detection_boxes'].numpy().tolist()
+    detection_scores = prediction['detection_scores'].numpy().tolist()[0]
+    detection_classes = prediction['detection_classes'].numpy().tolist()[0]
 
-        detection_box = detection_boxes[0][0]
+    detection_box = detection_boxes[0][0]
 
-        image_with_detections = image_np;
+    image_with_detections = image_np
 
-        width, height = image.size
-        green = (0, 255, 0)
+    width, height = image.size
+    green = (0, 255, 0)
 
-        y1 = int(height * detection_box[0])
-        x1 = int(width * detection_box[1])
-        y2 = int(height * detection_box[2])
-        x2 = int(width * detection_box[3])
-        image_with_detections = cv2.rectangle(
-            image_with_detections,
-            (x1, y1),
-            (x2, y2),
-            green,
-            3
-        )
+    y1 = int(height * detection_box[0])
+    x1 = int(width * detection_box[1])
+    y2 = int(height * detection_box[2])
+    x2 = int(width * detection_box[3])
+    image_with_detections = cv2.rectangle(
+        image_with_detections,
+        (x1, y1),
+        (x2, y2),
+        green,
+        3
+    )
 
-        detection_label = "{0} {1}".format(labels[detection_classes[0]], detection_scores[0])
-        label_padding = 5
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(
-            image_with_detections,
-            detection_label,
-            (x1, y1 - label_padding),
-            font,
-            0.5,
-            green,
-            1,
-            cv2.LINE_AA
-        )
+    detection_label = "{0} {1}".format(
+        labels[detection_classes[0]], detection_scores[0])
+    label_padding = 5
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(
+        image_with_detections,
+        detection_label,
+        (x1, y1 - label_padding),
+        font,
+        0.5,
+        green,
+        1,
+        cv2.LINE_AA
+    )
 
-        image_with_detections_pil = PIL.Image.fromarray(image_with_detections)
+    image_with_detections_pil = PIL.Image.fromarray(image_with_detections)
 
-        img_byte_arr = io.BytesIO()
-        image_with_detections_pil.save(img_byte_arr, format='JPEG')
-        img_byte_arr = img_byte_arr.getvalue()
-        return img_byte_arr
+    img_byte_arr = io.BytesIO()
+    image_with_detections_pil.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+    return img_byte_arr
+
+
+def send_email(max_index, detections, image_filename, img_byte_arr, duration):
+    # Send an email notification for this event
+    m = ""
+    for c in detections:
+        label_display_name = labels[c]
+        detection_line = label_display_name + ": " + str(detections[c])
+        m = m + detection_line
+
+    subject = 'Motion detected - {0} {1}'.format(
+        labels[max_index], detections[max_index])
+    cid = image_filename + uuid.uuid4().hex
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = subject
+    message["From"] = message_from
+    message["To"] = message_to
+
+    text = """\
+Motion detected
+        """
+    html = """\
+<p><pre>{0}</pre></p>
+<p>Took: {2}</p>
+<p><img src="cid:{1}"></p>
+<hr/>
+        """
+    plain_part = MIMEText(text, "plain")
+    html_part = MIMEText(html.format(m, cid, duration), "html")
+
+    image_attachment = MIMEImage(img_byte_arr, _subtype="jpeg")
+    image_attachment.add_header(
+        'Content-Disposition', 'attachment; filename={0}'.format(image_filename))
+    image_attachment.add_header('Content-ID', '<{}>'.format(cid))
+
+    message.attach(plain_part)
+    message.attach(html_part)
+    message.attach(image_attachment)
+
+    server = smtplib.SMTP(smtp_server, 587)
+    server.login(smtp_user, smtp_password)
+    server.sendmail(message_from, message_to, message.as_string())
+    logging.info("Sent notification: " + subject)
 
 
 def on_message(client, userdata, msg):
@@ -190,7 +248,7 @@ def on_message(client, userdata, msg):
     # Decode the image payload
     base64_image = message['image']
     image = PIL.Image.open(BytesIO(base64.b64decode(base64_image)))
-    image_filename =  message['image_file'].rsplit('/', 1)[1]
+    image_filename = message['image_file'].rsplit('/', 1)[1]
 
     image_np = numpy.array(image)
 
@@ -202,7 +260,8 @@ def on_message(client, userdata, msg):
     detection_classes = prediction['detection_classes'].numpy().tolist()[0]
 
     # Merge detection class with scores
-    detected_classes = numpy.array(list(zip(detection_classes, detection_scores)))
+    detected_classes = numpy.array(
+        list(zip(detection_classes, detection_scores)))
 
     from collections import defaultdict
 
@@ -212,14 +271,15 @@ def on_message(client, userdata, msg):
         s = detection_scores[i]
         i = detections[c]
         if s > i:
-           detections[c] = s
-
+            detections[c] = s
 
     annotated_image_byte_arr = annotateImage(prediction, image, image_np)
 
     # Publish notifications for strong class detections and find the max detection strength
-    base64_annotated_image = base64.b64encode(annotated_image_byte_arr).decode("ascii")
-    send_detection_message(detections, base64_image, base64_annotated_image)
+    base64_annotated_image = base64.b64encode(
+        annotated_image_byte_arr).decode("ascii")
+    send_detection_message(detections, base64_image,
+                           base64_annotated_image, duration)
 
     # Find the best detection
     max = 0
@@ -229,59 +289,18 @@ def on_message(client, userdata, msg):
             max = detections[c]
             max_index = c
 
-    # Schedule broadcast of a non motion message 
+    # Schedule broadcast of a non motion message
     logging.info("Scheduling send zeros")
     t = threading.Timer(30, send_zeros)
     t.start()
 
-
     logging.info("Best prediction is: " + str(max))
     if (max > 0.20) & (time.time() - last_sent > 60):
-        # Send an email notification for this event
-        # Slack would probably be more immediate 
-        m = ""
-        for c in detections:
-            label_display_name = labels[c]
-            detection_line = label_display_name + ": " + str(detections[c])
-            m = m + detection_line
-
-        subject = 'Motion detected - {0} {1}'.format(labels[max_index], detections[max_index])
-        cid = image_filename + uuid.uuid4().hex
-
-        message = MIMEMultipart("alternative")
-        message["Subject"] = subject
-        message["From"] = message_from
-        message["To"] = message_to
-
-        text = """\
-Motion detected
-        """
-        html = """\
-<p><pre>{0}</pre></p>
-<p>Took: {2}</p>
-<p><img src="cid:{1}"></p>
-<hr/>
-        """
-        plain_part = MIMEText(text, "plain")
-        html_part = MIMEText(html.format(m, cid, duration), "html")
-
-        img_byte_arr = annotated_image_byte_arr 
-
-        image_attachment = MIMEImage(img_byte_arr, _subtype="jpeg")
-        image_attachment.add_header('Content-Disposition', 'attachment; filename={0}'.format(image_filename))
-        image_attachment.add_header('Content-ID', '<{}>'.format(cid))
-
-        message.attach(plain_part)
-        message.attach(html_part)
-        message.attach(image_attachment)
-
-        server = smtplib.SMTP(smtp_server, 587)
-        server.login(smtp_user, smtp_password)
-        server.sendmail(message_from, message_to, message.as_string())
-        logging.info("Sent notification: " + subject)
-
+        send_email(max_index, detections, image_filename,
+                   annotated_image_byte_arr, duration)
         # Update rate limit watermark
         last_sent = time.time()
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -292,4 +311,3 @@ client.connect(broker, port, 60)
 send_zeros()
 
 client.loop_forever()
-
