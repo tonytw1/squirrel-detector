0d26641e5bef2410f64d91a079c0f395c5754fa6
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-08-29 12:04:41 +0100

    Initial tflite inference.
    Applied normalisation / metadata as per
    https://colab.research.google.com/github/tensorflow/models/blob/master/research/object_detection/colab_tutorials/convert_odt_model_to_TFLite.ipynb
    but it doesn't appear to significately effect the outcome.
    
    For squirrel.jpg test image, the production model gives
    squirrel: 0.9971960783004761cat: 0.2646138072013855fox: 0.09640860557556152magpie: 0.03315169736742973pigeon: 0.028847934678196907
    Took: 0.10987186431884766
    
    TF lite model gives:
    time: 571.947ms
    0.996104: 0
    0.243949: 1
    0.110767: 2
    0.074861: 3
    0.060784: 4
    
    Mostly fox also gives consistant results.

--- convert.py
+++ convert.py
@@ -2,9 +2,43 @@ import tensorflow as tf
 
 # Convert the model
 converter = tf.lite.TFLiteConverter.from_saved_model('models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/tflite/saved_model') # path to the SavedModel directory
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
 
-with open('models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/tflite/squirrelnet.tflite', 'wb') as f:
+# print("Writing tflife model")
+TF_LITE_MODEL_PATH = 'models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/tflite/squirrelnet.tflite'
+with open(TF_LITE_MODEL_PATH, 'wb') as f:
 	f.write(tflite_model)
 
+# Convert the labels to tflite toolings format
+# TODO hard dependencies; converted manually
+#LABEL_PATH = 'retraining/data/tf_label_map.pbtxt'
+TF_LITE_LABEL_PATH = 'models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/tflite/squirrelnet_label_map.txt'
+
+#print("Converting labels to label map text file")
+#from object_detection.utils import label_map_util
+
+#category_index = label_map_util.create_category_index_from_labelmap(LABEL_PATH)
+#f = open(TF_LITE_LABEL_PATH, 'w')
+#for class_id in range(1, 91):
+#  if class_id not in category_index:
+#    f.write('???\n')
+#    continue
+#  name = category_index[class_id]['name']
+#  f.write(name+'\n')
+#f.close()
+
+# Normalise the model
+print("Normalising model")
+from tflite_support.metadata_writers import object_detector
+from tflite_support.metadata_writers import writer_utils
+
+writer = object_detector.MetadataWriter.create_for_inference(
+    writer_utils.load_file(TF_LITE_MODEL_PATH),
+    input_norm_mean=[127.5],
+    input_norm_std=[127.5],
+    label_file_paths=[TF_LITE_LABEL_PATH]
+    )
+
+writer_utils.save_file(writer.populate(), TF_LITE_MODEL_PATH + ".normalised")
 print("Done")
--- detect_tflite.py
+++ detect_tflite.py
@@ -1,40 +1,56 @@
 #!/usr/bin/python3
-
+# See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/examples/python/label_image.py
 import sys
 import PIL.Image
 import numpy
-import tensorflow as tf
+import time
 
 image_file = sys.argv[1]
 image = PIL.Image.open(image_file)
 
-# Tflite mode needs the image to be resized for it
-size = 640, 640
-resized_image = image.thumbnail(size)
-image = PIL.Image.open(image_file)
-resized_image = image.resize(size)
-
-# Convert our image to a numpy array
-np_image = numpy.array(resized_image, dtype = numpy.float32) # This input type is different to detect.py
-
-# Convert import image to a tensor
-input_tensor = tf.convert_to_tensor(np_image)
-input_tensor = input_tensor[tf.newaxis, ...]
-
+import tensorflow as tf
 
 # Load the tflite model into an interpreter
-interpreter = tf.lite.Interpreter(model_path = 'models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/tflite/squirrelnet.tflite')
+interpreter = tf.lite.Interpreter(model_path = 'models/squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/tflite/squirrelnet.tflite.normalised')
 interpreter.allocate_tensors()
+print("Interpreter input details")
+print(interpreter.get_input_details())
 
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-input_shape = input_details[0]['shape']
-interpreter.set_tensor(input_details[0]['index'], input_tensor)
+# Tflite mode needs the image to be resized for it
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+resized_image = image.resize((width, height))
+input_data = numpy.expand_dims(resized_image, axis=0)
+
+# Convert input to floating point if this model requires them
+floating_model = input_details[0]['dtype'] == numpy.float32
+print("Is floating model: ", floating_model)
+if floating_model:
+    input_data = (numpy.float32(input_data) - 127.5) / 127.5	# TODO hardcoded input_mean and input_std normalisation constants; can these be extracted from the model?
+
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+# Invoke
+start_time = time.time()
 interpreter.invoke()
+stop_time = time.time()
+
+print('time: {:.3f}ms'.format((stop_time - start_time) * 1000))
+
 
 # The function `get_tensor()` returns a copy of the tensor data.
 # Use `tensor()` in order to get a pointer to the tensor.
 output_data = interpreter.get_tensor(output_details[0]['index'])
-print(output_data)
+
+results = numpy.squeeze(output_data)
+top_k = results.argsort()[-5:][::-1]
+for i in top_k:
+	if floating_model:
+		print('{:08.6f}: {}'.format(float(results[i]), i))
+	else:
+      		print('{:08.6f}: {}'.format(float(results[i] / 255.0), i))
+
