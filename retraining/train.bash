1d0d4cc87e6be113cc954e3f970a457bf25effcd
Tony McCrae <tony@eelpieconsulting.co.uk>
2022-08-31 16:14:49 +0100

    Model specfic pipeline and checkpoint file and folder; makes room for training alterative models

--- retraining/train.bash
+++ retraining/train.bash
@@ -1,4 +1,4 @@
 # Where models/ conatains a check out of https://github.com/tensorflow/models.git
 # and pretrained_models/ contains and extract of http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz
-python3 /models/research/object_detection/model_main_tf2.py --pipeline_config_path=squirrelnet_pipeline.config --model_dir=squirrelnet --alsologtostderr
+python3 /models/research/object_detection/model_main_tf2.py --pipeline_config_path=squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8_pipeline.config --model_dir=squirrelnet_ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8 --alsologtostderr
 
